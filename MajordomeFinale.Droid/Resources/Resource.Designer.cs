baba48d1591edba799efdde501cd169ceac6e461
Nathan DESCOMBES <descombes.n@gmail.com>
2017-03-16 14:30:32 +0100

    Test Restaurant

--- MajordomeFinale.Droid/Resources/Resource.Designer.cs
+++ MajordomeFinale.Droid/Resources/Resource.Designer.cs
@@ -48,13 +48,16 @@ namespace MajordomeFinale.Droid
 			public const int Icon = 2130837504;
 			
 			// aapt resource value: 0x7f020001
-			public const int Majordome_Logo = 2130837505;
+			public const int imad = 2130837505;
 			
 			// aapt resource value: 0x7f020002
-			public const int Restaurant = 2130837506;
+			public const int Majordome_Logo = 2130837506;
 			
 			// aapt resource value: 0x7f020003
-			public const int Route = 2130837507;
+			public const int Restaurant = 2130837507;
+			
+			// aapt resource value: 0x7f020004
+			public const int Route = 2130837508;
 			
 			static Drawable()
 			{
@@ -69,23 +72,29 @@ namespace MajordomeFinale.Droid
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f050002
-			public const int LoginEntry = 2131034114;
+			// aapt resource value: 0x7f050004
+			public const int LoginEntry = 2131034116;
+			
+			// aapt resource value: 0x7f050007
+			public const int MyButton = 2131034119;
 			
 			// aapt resource value: 0x7f050005
-			public const int MyButton = 2131034117;
+			public const int PasswordEntry = 2131034117;
 			
-			// aapt resource value: 0x7f050003
-			public const int PasswordEntry = 2131034115;
-			
-			// aapt resource value: 0x7f050000
-			public const int WelcomeLogo = 2131034112;
-			
-			// aapt resource value: 0x7f050004
-			public const int loginButton = 2131034116;
+			// aapt resource value: 0x7f050002
+			public const int WelcomeLogo = 2131034114;
 			
 			// aapt resource value: 0x7f050001
-			public const int textView1 = 2131034113;
+			public const int imageView1 = 2131034113;
+			
+			// aapt resource value: 0x7f050000
+			public const int linearLayout1 = 2131034112;
+			
+			// aapt resource value: 0x7f050006
+			public const int loginButton = 2131034118;
+			
+			// aapt resource value: 0x7f050003
+			public const int textView1 = 2131034115;
 			
 			static Id()
 			{
@@ -101,16 +110,19 @@ namespace MajordomeFinale.Droid
 		{
 			
 			// aapt resource value: 0x7f030000
-			public const int Login = 2130903040;
+			public const int Account = 2130903040;
 			
 			// aapt resource value: 0x7f030001
-			public const int Main = 2130903041;
+			public const int Login = 2130903041;
 			
 			// aapt resource value: 0x7f030002
-			public const int Restaurant = 2130903042;
+			public const int Main = 2130903042;
 			
 			// aapt resource value: 0x7f030003
-			public const int Route = 2130903043;
+			public const int Restaurant = 2130903043;
+			
+			// aapt resource value: 0x7f030004
+			public const int Route = 2130903044;
 			
 			static Layout()
 			{
